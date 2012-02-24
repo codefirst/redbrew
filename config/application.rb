aebef710f888dcf116e08a979bc321662c85bfbc
suer <suetsugu.r@gmail.com>
2012-02-24 23:32:43 +0900

    add haml config

--- config/application.rb
+++ config/application.rb
@@ -61,5 +61,9 @@ module Redbrew
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.template_engine :haml
+    end
   end
 end
