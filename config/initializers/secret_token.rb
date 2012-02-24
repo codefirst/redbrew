dc398e781e2d2b3b2ce9a318c9d91069535af76a
suer <suetsugu.r@gmail.com>
2012-02-24 23:24:42 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redbrew::Application.config.secret_token = '8cab8f01f27a100f80c2707b21aa3270054dff361308ecab84490d2b8d6d661452a9f472aa5c8d5ea533d1dbee7385245db27e372baf6eaffbd9c304afee6c58'
