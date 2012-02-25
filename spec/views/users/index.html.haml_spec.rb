63a5a812ddf13d3b5b21e7dc4aa86124b8b96376
suer <suetsugu.r@gmail.com>
2012-02-25 16:52:49 +0900

    remove user modification method

--- spec/views/users/index.html.haml_spec.rb
+++ spec/views/users/index.html.haml_spec.rb
@@ -23,6 +23,6 @@ describe "users/index" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Github Name".to_s, :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Icon Url".to_s, :count => 2
+    assert_select "tr>td", :text => "Icon".to_s, :count => 2
   end
 end
