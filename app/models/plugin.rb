c59d8babb66fa96dc8dbfe18b40f51fa4f8b8f83
suer <suetsugu.r@gmail.com>
2012-02-28 23:11:34 +0900

    find by plugin name refs #1038

--- app/models/plugin.rb
+++ app/models/plugin.rb
@@ -1,4 +1,14 @@
 class Plugin < ActiveRecord::Base
   belongs_to :create_user, :class_name => 'User', :foreign_key => 'create_user_id'
   belongs_to :update_user, :class_name => 'User', :foreign_key => 'update_user_id'
+
+  def self.find(*args)
+    if args.first && args.first.is_a?(String) && !args.first.match(/^\d*$/)
+      project = find_by_name(*args)
+      raise ActiveRecord::RecordNotFound, "Couldn't find Project with identifier=#{args.first}" if project.nil?
+      project
+    else
+      super
+    end
+  end
 end
