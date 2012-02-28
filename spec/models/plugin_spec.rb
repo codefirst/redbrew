01e7940408bd16d4a5594053ea050387266b63bb
suer <suetsugu.r@gmail.com>
2012-02-28 23:28:53 +0900

    uniqueness test refs #1039

--- spec/models/plugin_spec.rb
+++ spec/models/plugin_spec.rb
@@ -1,24 +1,40 @@
 require 'spec_helper'
 
 describe Plugin do
-  before do
-    @plugin = Plugin.new(
-      :id => 1,
-      :name => 'some plugin',
-      :url => 'http://example.com/plugin/',
-      :desription => 'a plugin for redmine',
-      :formula => 'script')
-    @user = User.new(
-      :id => 1,
-      :name => 'test user',
-      :github_name => 'test',
-      :icon_url => 'http://example.com/icon.png')
-    @plugin.create_user = @user
-    @plugin.update_user = @user
+  context 'association' do
+    before do
+      @plugin = Plugin.new(
+        :id => 1,
+        :name => 'some plugin',
+        :url => 'http://example.com/plugin/',
+        :desription => 'a plugin for redmine',
+        :formula => 'script')
+      @user = User.new(
+        :id => 1,
+        :name => 'test user',
+        :github_name => 'test',
+        :icon_url => 'http://example.com/icon.png')
+      @plugin.create_user = @user
+      @plugin.update_user = @user
+    end
+
+    subject { @plugin }
+
+    its(:create_user) { should == @user }
+    its(:update_user) { should == @user }
   end
 
-  subject { @plugin }
+  context 'uniqueness' do
+    before do
+      @plugin1 = Plugin.new(:name => 'test')
+      @plugin2 = Plugin.new(:name => 'test')
+    end
 
-  its(:create_user) { should == @user }
-  its(:update_user) { should == @user }
+    it 'error raised when save same plugin' do
+      lambda {
+        @plugin1.save!
+        @plugin2.save!
+      }.should raise_error
+    end
+  end
 end
