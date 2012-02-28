4ea2f79a63a1aeacdc6d5b6c8d5c81dabb206194
suer <suetsugu.r@gmail.com>
2012-02-28 23:17:46 +0900

    an unique constraint refs #1038

--- app/models/plugin.rb
+++ app/models/plugin.rb
@@ -1,4 +1,6 @@
 class Plugin < ActiveRecord::Base
+  validates :name, :uniqueness => true
+
   belongs_to :create_user, :class_name => 'User', :foreign_key => 'create_user_id'
   belongs_to :update_user, :class_name => 'User', :foreign_key => 'update_user_id'
 
